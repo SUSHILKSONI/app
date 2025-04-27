d5d5bf0caa8b50651b30f83d8403068118d75dd8
SUSHIL SONI <sushilksoni147@gmail.com>
2025-04-27 19:25:10 +0530

    Update dashboard.py

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import pickle
+import os
 
+# Load the trained model
+model = pickle.load(open(os.path.join('model', 'salary_predictor.pkl'), 'rb'))
+
+st.title('Salary Predictor App')
+
+# User inputs
+exp = st.number_input('Experience (in years)', min_value=0, step=1)
+test_score = st.number_input('Test Score (out of 10)', min_value=0, max_value=10, step=1)
+interview_score = st.number_input('Interview Score (out of 10)', min_value=0, max_value=10, step=1)
+
+# Predict button
+if st.button('Predict Salary'):
+    prediction = model.predict([[exp, test_score, interview_score]])
+    st.success(f'Predicted Salary: ${int(prediction[0])}')
